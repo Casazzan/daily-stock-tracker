ce6da4577ff1448b1ad21843c8b3f6560e1763af
Casazzan <nrcasazza@gmail.com>
2020-08-07 19:34:26 -0500

    Updates

---
+++ precache-manifest.ed0ae12f9606d44906ab6a74420e2c1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad12cbc3df73f700d54e9d95f0d222b2",
+    "url": "/daily-stock-tracker/index.html"
+  },
+  {
+    "revision": "597dde8aa2f2f829c22c",
+    "url": "/daily-stock-tracker/static/css/2.7d0b1304.chunk.css"
+  },
+  {
+    "revision": "c138e5c87a9e95b9089b",
+    "url": "/daily-stock-tracker/static/css/main.281cb9bb.chunk.css"
+  },
+  {
+    "revision": "597dde8aa2f2f829c22c",
+    "url": "/daily-stock-tracker/static/js/2.4faffd7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/daily-stock-tracker/static/js/2.4faffd7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c138e5c87a9e95b9089b",
+    "url": "/daily-stock-tracker/static/js/main.24bdbccc.chunk.js"
+  },
+  {
+    "revision": "4984daa1954f9e91b34c",
+    "url": "/daily-stock-tracker/static/js/runtime-main.02b0c2d4.js"
+  }
+]);
